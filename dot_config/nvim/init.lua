714d75b1ff2b4be8637c7152bb515ef5fd2a7011
Mikołaj Data <miki3867@gmail.com>
2023-09-22 17:44:13 +0200

    Polybar: fix labels

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,14 +1,18 @@
-if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
+if vim.loader and vim.fn.has("nvim-0.9.1") == 1 then
+  vim.loader.enable()
+end
 
-for _, source in ipairs {
+for _, source in ipairs({
   "astronvim.bootstrap",
   "astronvim.options",
   "astronvim.lazy",
   "astronvim.autocmds",
   "astronvim.mappings",
-} do
+}) do
   local status_ok, fault = pcall(require, source)
-  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+  if not status_ok then
+    vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
+  end
 end
 
 if astronvim.default_colorscheme then
