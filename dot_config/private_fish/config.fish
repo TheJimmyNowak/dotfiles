5fd2a4bd06958e41d02e6c6956627f2b50d5b95a
Mikołaj Data <miki3867@gmail.com>
2023-10-23 16:59:30 +0200

    Create sudo -e alias

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,8 +6,10 @@ starship init fish | source
 alias la  "exa -a --icons"
 alias dobranoc  "shutdown -h now"
 alias n "nvim"
+alias ns "sudo -e"
 alias ls "exa --icons"
 alias ce "chezmoi edit"
+alias r "ranger"
 
 set -U fish_greeting
 set -Ux EDITOR nvim
