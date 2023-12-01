3f9ca35ba7430d42d24614b317b0291cf830cdc5
Mikołaj Data <miki3867@gmail.com>
2023-12-01 21:37:55 +0100

    Add git config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -10,6 +10,8 @@ alias ns "sudo -e"
 alias ls "exa --icons"
 alias ce "chezmoi edit"
 alias r "ranger"
+alias g "git"
+alias peaclock="peaclock config-dir ~/.config/peaclock/"
 
 set -U fish_greeting
 set -gx EDITOR nvim
